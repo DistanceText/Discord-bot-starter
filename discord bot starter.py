ae1075a79a0116e6324611957aca137271caa529
DistanceText <DistanceText@gmail.com>
2023-04-19 10:37:48 +0200

    first commit

---
+++ discord bot starter.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from discord import member
+from discord.ext.commands import has_permissions, MissingPermissions
+import datetime
+from typing import List
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+clientprefix = '^'
+BotRpc = "RPC"
+client = commands.Bot(command_prefix=clientprefix, intents=intents,status=discord.Status.idle)
+
+@client.event
+async def on_ready():
+    print('Bot working as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=BotRpc),status=discord.Status.do_not_disturb)
+    print('rpc set! at ', str(datetime.datetime.now))
+    print('Bot is now working! started at: ', str(datetime.datetime.now))
+    print('--------------------------------------------------------------')
+
+
+client.run('YOUR TOKEN GOES HERE')
