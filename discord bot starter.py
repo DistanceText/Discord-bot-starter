57465e2932e567d48ee0ea5ad76f9871c8f2bd54
DistanceText <123662825+DistanceText@users.noreply.github.com>
2023-04-22 13:32:16 +0200

    Update discord bot starter.py

--- discord bot starter.py
+++ discord bot starter.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord import member
 from discord.ext.commands import has_permissions, MissingPermissions
+from apikeys import token
 import datetime
 from typing import List
 intents = discord.Intents.default()
@@ -9,14 +10,15 @@ intents.members = True
 intents.message_content = True
 clientprefix = '^'
 BotRpc = "RPC"
+startuptime = datetime.datetime.utcnow()
 client = commands.Bot(command_prefix=clientprefix, intents=intents,status=discord.Status.idle)
 
 @client.event
 async def on_ready():
     print('Bot working as {0.user}'.format(client))
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=BotRpc),status=discord.Status.do_not_disturb)
-    print('rpc set! at ', str(datetime.datetime.now))
-    print('Bot is now working! started at: ', str(datetime.datetime.now))
+    print('rpc set! at ', startuptime)
+    print('Bot is now working! started at: ', startuptime)
     print('--------------------------------------------------------------')
 
 @client.command()
